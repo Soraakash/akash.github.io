0189e229f6a19e2d785b3fa364187ba5040d5b1b
Akash Kumar <88193959+Soraakash@users.noreply.github.com>
2023-01-28 18:18:59 +0530

    Update service.js

--- service.js
+++ service.js
@@ -7,12 +7,12 @@ closemenu.addEventListener("click", close);
 
 function show() {
   mainmenu.style.display = "flex";
-  mainmenu.style.top = "0";
+  mainmenu.style.right = "0";
   closemenu.style.display = "block";
   openmenu.style.display = "none";
 }
 function close() {
-  mainmenu.style.top= "-100%";
+  mainmenu.style.right= "-100%";
   closemenu.style.display = "none";
   openmenu.style.display = "block";
 }
